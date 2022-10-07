644f6ecdf954b3d6d53ea9d6df3d814e45c6b7a2
Yulia <yulia1201556@gmail.com>
2022-10-08 00:03:02 +0300

    Список простых множителей заданного натурального числа

--- Task/002.py
+++ Task/002.py
@@ -2,21 +2,17 @@
 #  которая составит список простых множителей числа N.
 
 
-def prime_factors(count):
-    factors = list()
-    while count % 2 == 0 or count % 3 == 0 or count % 5 == 0:
-        if count % 2 == 0:
-            factors.append(2)
-            count /= 2
-        elif count % 3 == 0:
-            factors.append(3)
-            count /= 3
-        elif count % 5 == 0:
-            factors.append(5)
-            count /= 5
-    if count > 1:
-        factors.append(round(count))
-    print(f'Простые множители числа N:', factors)
+def prime_list(num):
+    list = []
+    div = 2
+    while num > 1:
+        if num % div == 0:
+            list.append(div)
+            num //= div
+        else:
+            div += 1
+    return list        
 
-print('Задайте натуральное число N:')
-prime_factors(int(input()))
+
+print("Задайте натуральное число:")
+print(prime_list(int(input())))
