8e84ed3b37b6d6afa418d30d80b1550cc982cbfb
Yulia <yulia1201556@gmail.com>
2022-10-10 02:17:14 +0300

    Выводим список неповторяющихся элементов

--- Task/003.py
+++ Task/003.py
@@ -6,32 +6,47 @@
 # 1113384455229 -> [8,9]
 # 1115566773322 -> []
 
-import random
 
-print("Введите число элементов списка: ")
-len_list = int(input())
-list = []
+# import random
 
-if len_list < 0:
-       print("Error")
+# print("Введите число элементов списка: ")
+# len_list = int(input())
+# list = []
+
+# if len_list < 0:
+#        print("Error")
       
-for i in range (len_list):
-    list.append(random.randint(1, len_list))
-print(list)
+# for i in range (len_list):
+#     list.append(random.randint(1, len_list))
+# print(list)
 
 
-def uniq_elem(list):
+# def uniq_elem(list):
     
-    result = []
-    dict = {}.fromkeys(list, 0)
+#     result = []
+#     dict = {}.fromkeys(list, 0)
 
-    for i in list:
-        dict[i] += 1
+#     for i in list:
+#         dict[i] += 1
 
-    for j in dict:
-        if dict[j] == 1:
-            result.append(j)
+#     for j in dict:
+#         if dict[j] == 1:
+#             result.append(j)
 
-    return result
+#     return result
 
-print(uniq_elem(list))
+# print(uniq_elem(list))
+
+# import numpy 
+
+# from collections import Counter 
+
+# items = numpy.random.randint(10, size = 10)
+# items_set = list(items[:10])
+# print(*items_set)
+
+# dictionary = Counter(items_set)
+# result = [i for i in dictionary if dictionary[i] == 1]
+
+# print(*result)
+# не работает
