42cf6cf3c8cadbf663d8b6541eed39f24c482764
Yulia <yulia1201556@gmail.com>
2022-10-10 02:17:59 +0300

    Выводим список неповторяющихся элементов

--- Task/003.py
+++ Task/003.py
@@ -7,35 +7,35 @@
 # 1115566773322 -> []
 
 
-# import random
+import random
 
-# print("Введите число элементов списка: ")
-# len_list = int(input())
-# list = []
+print("Введите число элементов списка: ")
+len_list = int(input())
+list = []
 
-# if len_list < 0:
-#        print("Error")
+if len_list < 0:
+       print("Error")
       
-# for i in range (len_list):
-#     list.append(random.randint(1, len_list))
-# print(list)
+for i in range (len_list):
+    list.append(random.randint(1, len_list))
+print(list)
 
 
-# def uniq_elem(list):
+def uniq_elem(list):
     
-#     result = []
-#     dict = {}.fromkeys(list, 0)
+    result = []
+    dict = {}.fromkeys(list, 0)
 
-#     for i in list:
-#         dict[i] += 1
+    for i in list:
+        dict[i] += 1
 
-#     for j in dict:
-#         if dict[j] == 1:
-#             result.append(j)
+    for j in dict:
+        if dict[j] == 1:
+            result.append(j)
 
-#     return result
+    return result
 
-# print(uniq_elem(list))
+print(uniq_elem(list))
 
 # import numpy 
 
